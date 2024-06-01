7c62cb6670a0e9c9ecb002c3c47df2fd0895e0b3
umerhussain2 <umerhussain240898@gmail.com>
2024-06-01 16:40:29 +0500

    some modification

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import { MyContext } from "./context/context";
 import "./App.css";
-import Comp1 from "./components/Comp1";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -23,42 +21,34 @@ function App() {
   };
 
   return (
-    <>
-      <MyContext.Provider value={count}>
-        <main className="container">
-          <div className="wrapper">
-            <Comp1 />
-            <h1 className="heading">counter</h1>
+    <main className="container">
+      <div className="wrapper">
+        <h1 className="heading">counter</h1>
 
-            <div className="display-wrapper">
-              <div className="display">{count}</div>
-            </div>
-            <div className="buttons-wrapper">
-              <button
-                onClick={() => decrement(1)}
-                className="action-button value-decrement"
-              >
-                -
-              </button>
+        <div className="display-wrapper">
+          <div className="display">{count}</div>
+        </div>
+        <div className="buttons-wrapper">
+          <button
+            onClick={() => decrement(1)}
+            className="action-button value-decrement"
+          >
+            -
+          </button>
 
-              <button
-                onClick={() => reset()}
-                className="action-button value-reset"
-              >
-                Reset
-              </button>
+          <button onClick={() => reset()} className="action-button value-reset">
+            Reset
+          </button>
 
-              <button
-                onClick={() => increment(1)}
-                className="action-button value-increment"
-              >
-                +
-              </button>
-            </div>
-          </div>
-        </main>
-      </MyContext.Provider>
-    </>
+          <button
+            onClick={() => increment(1)}
+            className="action-button value-increment"
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
--- src/components/Comp3.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import { MyContext } from "../context/context";
-
-const Comp3 = () => {
-  const x = useContext(MyContext);
-  return <div>{x}</div>;
-};
-
-export default Comp3;
